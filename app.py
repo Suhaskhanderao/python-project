d1de712689e17823b979186bdcdb84c4e3821d41
Suhaskhanderao <suhaskhanderao051@gmail.com>
2025-01-25 07:24:30 +0000

    done1

--- app.py
+++ app.py
@@ -1,18 +1,7 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-# Root route
-@app.route("/", methods=["GET"])
-def home():
-    return jsonify({"msg": "Welcome to the Flask App!"})
-
-# /hello route
-@app.route("/hello", methods=["GET"])
-def say_hello():
-    return jsonify({"msg": "Hello from Flask!"})
-
-if __name__ == "__main__":
-    # Debug should be disabled in production
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
+from flask import Flask
+app = Flask (_name_)
+@app.route ('/')
+def hello () :
+ return "HELLO, WORLD!"
+if _name_ == "_main_":
+ app.run (host="0.0.0.0", port=5000)
